9a124d0b57baf76c4d1825ce8d746bdfbec7e262
wish1989 <791859521@qq.com>
2016-04-04 20:14:35 +0800

    ExData_Plotting1

---
+++ plot1.R
@@ -0,0 +1,6 @@
+hp <- read.csv("household_power_consumption.txt",header = TRUE,skip = 66636,nrows = 2880,sep = ";",na.strings="?")
+hp_2 <- read.csv("household_power_consumption.txt",header = TRUE,nrows = 1,sep = ";")
+names(hp) <- names(hp_2)
+png(filename='plot1.png', width=480, height=480, units='px')
+hist(hp$Global_active_power,xlab = "Global Active Power (kilowatta)",main = "Global Active Power", col = "red")
+dev.off()
